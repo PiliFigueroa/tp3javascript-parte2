bb1ba2d2b3c0098885ee76fe58fa4337f0d815ca
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-01-06 23:25:01 -0300

    agrego archivos dom.js y methods.js

---
+++ methods.js
@@ -0,0 +1,9 @@
+const urlBase = 'https://5ff65d8d941eaf0017f379f0.mockapi.io';
+
+fetch(urlBase + '/users')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => console.log(error))
+
