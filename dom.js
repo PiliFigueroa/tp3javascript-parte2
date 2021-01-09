c9d1fb79160cbf3b20970ef88db1dd27ba3a09b1
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2021-01-08 23:54:00 -0300

    checkeo expresion regular y evento submit

--- dom.js
+++ dom.js
@@ -2,14 +2,12 @@
 
 const form = document.getElementById('form');
 const inputs = document.querySelectorAll('#form input');
-console.log(form)
-console.log(inputs)
 
 const expressions = {
-    name: /^[a-zA-ZÀ-ÿ\s]{1,50}$/, // Letras y espacios, pueden llevar acentos.
+    name: /^[a-zA-ZÀ-ÿ\s]{1,50}$/,
     address: /[a-zA-Z0-9_+-]{1,60}/,
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    phone: /[0-9]{3,13}/ // 1 a 13 numeros. //agregarle poder poner Solo números, espacios y/o guiones medios}$/  []
+    email: /^[a-zA-Z0-9_+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+$/,
+    phone: /[0-9]{3,13}/
 }
 
 const fields = {
@@ -63,16 +61,14 @@ form.addEventListener('submit', (e) => {
 
     if (fields.name && fields.address && fields.email && fields.phone) {
         form.reset();
-
+        //console.log('formulario enviado');
         document.getElementById('form-message-success').classList.add('form-message-success-active');
+        document.getElementById('form-message').style.display = 'none';
         setTimeout(() => {
             document.getElementById('form-message-success').classList.remove('form-message-success-active');
         }, 5000);
-
-        document.querySelectorAll('.form-group-correct').forEach((icon) => {
-            icon.classList.remove('form-group-correct');
-        });
     } else {
         document.getElementById('form-message').classList.add('form-message-active');
+        //console.log('formulario no enviado');
     }
 });
