673009bf7bb8ff8b5654497f4c6d7ceb61cc194a
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-01-10 00:07:16 -0300

    uso función fetch con método get y muestro datos de mock api en la tabla

--- methods.js
+++ methods.js
@@ -3,7 +3,13 @@ const urlBase = 'https://5ff65d8d941eaf0017f379f0.mockapi.io';
 fetch(urlBase + '/users')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        createTable(data)
     })
     .catch(error => console.log(error))
+
+// fetch (urlBase + '/users', {
+//     method: 'POST',
+//     headers: {'Content-Type': 'Application/json'
+//     }
+// })
 
